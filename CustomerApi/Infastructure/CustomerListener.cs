b91e59dfb441ebff3d094cded1f3e176214a8e5b
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2019-03-15 13:53:47 +0100

    Added UpdateCreditStanding and message listener

---
+++ CustomerApi/Infastructure/CustomerListener.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CustomerApi.Data;
+using CustomerApi.Models;
+using EasyNetQ;
+using Microsoft.Extensions.DependencyInjection;
+using SharedModels;
+
+namespace CustomerApi.Infrastructure
+{
+    public class CustomerListener
+    {
+        IServiceProvider provider;
+        readonly string connectionString;
+
+        // The service provider is passed as a parameter, because the class needs
+        // access to the product repository. With the service provider, we can create
+        // a service scope that can provide an instance of the product repository.
+        public CustomerListener(IServiceProvider provider, string connectionString)
+        {
+            this.provider = provider;
+            this.connectionString = connectionString;
+        }
+
+
+        public void Start()
+        {
+            //string connectionStr = "host=hare.rmq.cloudamqp.com;virtualHost=npaprqop;username=npaprqop;password=TnP46q2gwIcrbfebFLHTk1PGI8j3-vbA";
+
+            using (var bus = RabbitHutch.CreateBus(connectionString))
+            {
+                bus.Subscribe<OrderStatusChangedMessage>("customerApi",
+                    UpdateCreditStanding, x => x.WithTopic(OrderDTO.OrderStatus.completed.ToString()));
+
+                bus.Subscribe<OrderStatusChangedMessage>("customerApi",
+                    UpdateCreditStanding, x => x.WithTopic(OrderDTO.OrderStatus.shipped.ToString()));
+
+
+                // Block the thread so that it will not exit and stop subscribing.
+                lock (this)
+                {
+                    Monitor.Wait(this);
+                }
+            }
+
+        }
+
+        private void UpdateCreditStanding(OrderStatusChangedMessage message)
+        {
+            using (var scope = provider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var customerRepos = services.GetService<IRepository<Customer>>();
+
+                var customer = customerRepos.Get(message.Order.CustomerId);
+                if (message.Order.Status == OrderDTO.OrderStatus.shipped)
+                {
+                    customer.creditStanding = false;
+                } else if (message.Order.Status == OrderDTO.OrderStatus.paid) 
+                {
+                    customer.creditStanding = true;
+                }
+                customerRepos.Edit(customer);
+            }
+        }
+
+
+    }
+}
--- CustomerApi/Startup.cs
+++ CustomerApi/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomerApi.Data;
+using CustomerApi.Infrastructure;
+using CustomerApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,6 +20,9 @@ namespace CustomerApi
 {
     public class Startup
     {
+        readonly string cloudAMQPConnectionString =
+            "host=bear.rmq.cloudamqp.com;virtualHost=vwwmjixz;username=vwwmjixz;password=JvUd-WcdNGkmCaBQVUxykmM2NEb6R3nc";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +33,14 @@ namespace CustomerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // In-memory database:
+            services.AddDbContext<CustomerApiContext>(opt => opt.UseInMemoryDatabase("ProductsDb"));
+
+            // Register repositories for dependency injection
+            services.AddScoped<IRepository<Customer>, CustomerRepository>();
+
+            // Register database initializer for dependency injection
+            services.AddTransient<IDbInitializer, DbInitializer>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             
             services.AddSwaggerGen(c =>
@@ -37,6 +52,20 @@ namespace CustomerApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Initialize the database
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                // Initialize the database
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetService<CustomerApiContext>();
+                var dbInitializer = services.GetService<IDbInitializer>();
+                dbInitializer.Initialize(dbContext);
+            }
+
+            // Create a message listener in a separate thread.
+            Task.Factory.StartNew(() =>
+                new CustomerListener(app.ApplicationServices, cloudAMQPConnectionString).Start());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
