d56d0b1830c360346f3ef348baf470d9cefc856c
Jesper Noer <jesper@noerit.gl>
2019-03-22 10:11:14 +0100

    Disabling SMTP

--- OrderApi/Infrastructure/MailService.cs
+++ OrderApi/Infrastructure/MailService.cs
@@ -7,12 +7,15 @@ namespace OrderApi.Infrastructure
     {
         public void sendMessage(string to, string subject, string message)
         {
+            System.Console.WriteLine("Message has been sent to " + to);
+            /*
             var client = new SmtpClient("smtp.mailtrap.io", 2525)
             {
                 Credentials = new NetworkCredential("8290a89e1403cc", "ac9cbda0bbae65"),
                 EnableSsl = true
             };
             client.Send("OrderAPI@mail.com", to, subject, message);
+            */
         }
     }
 }
